e18e17ea346646cda08dd9717db9bcf0a5b28668
ChenLi <33598645+chenlicpp@users.noreply.github.com>
2018-11-09 22:58:31 +0800

    Update Beep 12500.cpp

--- Beep 12500.cpp
+++ Beep 12500.cpp
@@ -1,18 +1,9 @@
-#include<iostream>
-#include<iomanip>
-#include<cstring>
-#include<cmath>
 #include<windows.h>
-using namespace std;
+
 int main(){
 	while(1){
-		Beep(12500,10000);
-
-		
+		Beep(12500,10000);	
 	}
-
-
-system("pause");
 return 0;
 }
 
